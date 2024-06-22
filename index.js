d794c1edd4d43125cfe1c1b395a7e18f23e38128
Fernando Rodriguez <feransic@gmail.com>
2024-06-22 17:23:27 -0600

    Add Hash funtionality to passwords for DB

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import db from "./db.js";
+import bcrypt, { hash } from 'bcrypt';
 
 db.connect();
 const app = express();
 const port = 3000;
+const saltRounds = 12;
 
 //Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,16 +30,25 @@ app.get("/register", (req, res) => {
 //POST request
 app.post("/register", async (req, res) => {
   try {
+    //Tomamos los datos del usuario
     const { username } = req.body;
     const { password } = req.body;
+    //Hacemos peticion para verificar si el usuario existe
     const checkEmail = await db.query("SELECT * FROM users WHERE email = $1", [username]);
+    //revisamos si el usuario existe
     if (checkEmail.rows.length > 0) {
       res.send("This email has been already used. Try to logging in");
     } else {
-
-      await db.query(`INSERT INTO users (email, password) VALUES($1, $2)`, [username, password]);
-      res.render('secrets.ejs');
-
+      //Si el usuario no existe lo registramos en la base de datos y lo enviamos a la pagina principal
+      //Hash password
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if(err) {
+          console.log("Hash Error: ", err);
+        } else {
+          await db.query(`INSERT INTO users (email, password) VALUES($1, $2)`, [username, hash]);
+          res.render('secrets.ejs');
+        }
+      })
     }
   } catch (error) {
     console.log(error);
@@ -48,10 +59,21 @@ app.post("/login", async (req, res) => {
   const { username } = req.body;
   const { password } = req.body;
   const checkEmail = await db.query("SELECT * FROM users WHERE email = $1", [username]);
+  const dbPassword = checkEmail.rows[0].password;
   //Revisamos si el usuario esta logeado
-  if(checkEmail.rows.length > 0) {
-    //Revisamos si la contraseña es correcta
-      checkEmail.rows[0].password === password ? res.render("secrets.ejs") : res.send("Incorrect Password");
+  if (checkEmail.rows.length > 0) {
+    //Revisamos si la contraseña es correcta mediante bcrypt
+    bcrypt.compare(password, dbPassword, (err, password) => {
+      if(err) {
+        console.log("Hash compare error: ", err);
+      } else {
+        if(password) {
+          res.render("secrets.ejs");
+        } else {
+          res.send("Incorrect Password")
+        }
+      }
+    })
   } else {
     //Si no esta loggeado le mandamos un mensaje
     res.send("Email not registered yet. Register Now!");
