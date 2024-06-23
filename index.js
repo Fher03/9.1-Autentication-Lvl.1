7c0282539f8317541bcf413b7106ddb9dee489c9
Fernando Rodriguez <feransic@gmail.com>
2024-06-22 20:06:15 -0600

    Manage cookies for register and login pages

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import db from "./db.js";
 import bcrypt from 'bcrypt';
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
 
 db.connect();
 const app = express();
@@ -12,6 +15,18 @@ const saltRounds = 12;
 //Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(session({
+  secret: "TOPSECRETWORD",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60* 24
+  }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session());
 
 
 //HTTP request 
@@ -28,6 +43,20 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
+app.get("/secrets", (req, res) => {
+  console.log(req.user.id);
+  if (req.isAuthenticated()) {
+    res.render("secrets.ejs")
+  } else {
+    res.redirect("/login");
+  }
+})
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secrets",
+  failureRedirect: "/login"
+}));
+
 //POST request
 app.post("/register", async (req, res) => {
   try {
@@ -43,11 +72,15 @@ app.post("/register", async (req, res) => {
       //Si el usuario no existe lo registramos en la base de datos y lo enviamos a la pagina principal
       //Hash password
       bcrypt.hash(password, saltRounds, async (err, hash) => {
-        if(err) {
+        if (err) {
           console.log("Hash Error: ", err);
         } else {
-          await db.query(`INSERT INTO users (email, password) VALUES($1, $2)`, [username, hash]);
-          res.render('secrets.ejs');
+          const result = await db.query(`INSERT INTO users (email, password) VALUES($1, $2) RETURNING *`, [username, hash]);
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log(err);
+            res.redirect("/secrets");
+          })
         }
       })
     }
@@ -56,31 +89,42 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  const { username } = req.body;
-  const { password } = req.body;
-  const checkEmail = await db.query("SELECT * FROM users WHERE email = $1", [username]);
-  const dbPassword = checkEmail.rows[0].password;
-  //Revisamos si el usuario esta logeado
-  if (checkEmail.rows.length > 0) {
-    //Revisamos si la contraseña es correcta mediante bcrypt
-    bcrypt.compare(password, dbPassword, (err, password) => {
-      if(err) {
-        console.log("Hash compare error: ", err);
-      } else {
-        if(password) {
-          res.render("secrets.ejs");
-        } else {
-          res.send("Incorrect Password")
-        }
-      }
-    })
-  } else {
-    //Si no esta loggeado le mandamos un mensaje
-    res.send("Email not registered yet. Register Now!");
-  }
 
-});
+passport.use(new Strategy(async function verify(username, password, cb) {
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [username]);
+    const user = result.rows[0]
+    const dbPassword = user.password;
+    //Revisamos si el usuario esta logeado
+    if (result.rows.length > 0) {
+      //Revisamos si la contraseña es correcta mediante bcrypt
+      bcrypt.compare(password, dbPassword, (err, password) => {
+        if (err) {
+          return cb(err)
+        } else {
+          if (password) {
+            return cb(null, user)
+          } else {
+            return cb(null, false)
+          }
+        }
+      })
+    } else {
+      //Si no esta loggeado le mandamos un mensaje
+      return cb("User not found");
+    }
+  } catch (error) {
+    return cb(error);
+  }
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+})
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+})
 
 //Listening Server
 app.listen(port, () => {
