f706c6e0698f2a7e47362788d8ff324e670b023b
Fernando Rodriguez <feransic@gmail.com>
2024-06-25 14:36:41 -0600

    Add Google OAuth 2.0 Strategy to login and register

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import env from "dotenv";
-import GoogleStrategy from 'passport-google-oauth2'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2'
 
 db.connect();
 const app = express();
@@ -19,15 +19,19 @@ env.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+//Valores de sesion 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    maxAge: 1000 * 60 * 60* 24
+    maxAge: 1000 * 60 * 60 * 24
   }
 }))
 
+
+//inicializando passport
 app.use(passport.initialize())
 app.use(passport.session());
 
@@ -38,9 +42,21 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-app.get("auth/google", passport.authenticate("google", {
-  scope: ["profile", "email"],
-}))
+//Manejamos rutas de google
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
@@ -50,6 +66,7 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
+//Revisamos si esta autenticado para entrar a la plataforma
 app.get("/secrets", (req, res) => {
   if (req.isAuthenticated()) {
     res.render("secrets.ejs")
@@ -58,12 +75,19 @@ app.get("/secrets", (req, res) => {
   }
 })
 
-app.get("/logout", (req, res) => { 
+
+
+//Cerrramos sesion y enviamos al login
+app.get("/logout", (req, res) => {
+  req.logout((err) => {
+    console.log(err)
+  })
   res.render("login.ejs");
 })
 
 
 //POST request
+//Manejo de redireccion exitosa y fallida y estrategia local
 app.post("/login", passport.authenticate("local", {
   successRedirect: "/secrets",
   failureRedirect: "/login"
@@ -100,7 +124,7 @@ app.post("/register", async (req, res) => {
   }
 });
 
-//Estrategia Local
+//Definimos nuestra Estrategia Local
 passport.use("local", new Strategy(async function verify(username, password, cb) {
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [username]);
@@ -132,10 +156,19 @@ passport.use("local", new Strategy(async function verify(username, password, cb)
 passport.use("google", new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/secrets',
-  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+  callbackURL: `${process.env.APP_URL}/auth/google/secrets`,
 }, async (accessToken, refreshToken, profile, cb) => {
-  console.log(profile);
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [profile.email]);
+    if (result.rows.length === 0) {
+      const newUser = await db.query("INSERT INTO users (email, password) VALUES($1, $2)", [profile.email, "google"]);
+      return cb(null, newUser.rows[0])
+    } else {
+      return cb(null, result.rows[0])
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }))
 
 //Serialize and deserialize User 
